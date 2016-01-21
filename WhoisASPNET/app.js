90ca612fbf5bb5ce839a62ec42525c6cf8f5c3f5
jsakamoto <junichi.jp@gmail.com>
2016-01-21 23:42:10 +0900

    Implement URL parameters and HTML5 History API support.

--- WhoisASPNET/app.js
+++ WhoisASPNET/app.js
@@ -1,19 +1,34 @@
 var WhoisASPNET;
 (function (WhoisASPNET) {
     var MainController = (function () {
-        function MainController($resource, $cookies) {
+        function MainController($scope, $resource, $cookies, $location) {
+            var _this = this;
             this.$cookies = $cookies;
+            this.$location = $location;
             this.encoding = $cookies.get('encoding') || 'us-ascii';
             this.whoisApi = $resource('/api/whois/:query');
             this.encodings = $resource('/api/encodings').query(function () {
                 setTimeout(function () { $('select').material_select(); }, 0);
+            });
+            $scope.$watch(function () { return $location.url(); }, function () {
+                var urlParams = $location.search();
+                _this.query = urlParams.query;
+                _this.server = urlParams.server;
+                _this.encoding = urlParams.encoding || _this.encoding;
+                if ((_this.query || '') != '') {
+                    _this.response = _this.whoisApi.get({
+                        query: _this.query,
+                        server: _this.server,
+                        encoding: _this.encoding
+                    });
+                }
             });
         }
         MainController.prototype.executeQuery = function (isValidForm) {
             if (isValidForm == false)
                 return;
             this.$cookies.put('encoding', this.encoding, { expires: 'Thu, 30 Dec 9999 15:00:00 GMT' });
-            this.response = this.whoisApi.get({
+            this.$location.search({
                 query: this.query,
                 server: this.server,
                 encoding: this.encoding
@@ -24,5 +39,6 @@ var WhoisASPNET;
     WhoisASPNET.MainController = MainController;
     angular
         .module('app', ['ngResource', 'ngCookies'])
+        .config(["$locationProvider", function ($locationProvider) { return $locationProvider.html5Mode(true); }])
         .controller('mainController', MainController);
 })(WhoisASPNET || (WhoisASPNET = {}));
--- WhoisASPNET/app.ts
+++ WhoisASPNET/app.ts
@@ -12,11 +12,32 @@
 
         public response: WhoisResponse;
 
-        constructor($resource: ng.resource.IResourceService, private $cookies: ng.cookies.ICookiesService) {
+        constructor(
+            $scope: ng.IScope,
+            $resource: ng.resource.IResourceService,
+            private $cookies: ng.cookies.ICookiesService,
+            private $location: ng.ILocationService
+        ) {
             this.encoding = $cookies.get('encoding') || 'us-ascii';
+
             this.whoisApi = $resource<WhoisResponse>('/api/whois/:query');
             this.encodings = $resource<string>('/api/encodings').query(() => {
                 setTimeout(() => { ($('select') as any).material_select(); }, 0);
+            });
+
+            $scope.$watch(() => $location.url(), () => {
+                var urlParams = $location.search();
+                this.query = urlParams.query;
+                this.server = urlParams.server;
+                this.encoding = urlParams.encoding || this.encoding;
+
+                if ((this.query || '') != '') {
+                    this.response = this.whoisApi.get({
+                        query: this.query,
+                        server: this.server,
+                        encoding: this.encoding
+                    });
+                }
             });
         }
 
@@ -24,7 +45,8 @@
             if (isValidForm == false) return;
 
             this.$cookies.put('encoding', this.encoding, { expires: 'Thu, 30 Dec 9999 15:00:00 GMT' });
-            this.response = this.whoisApi.get({
+
+            this.$location.search({
                 query: this.query,
                 server: this.server,
                 encoding: this.encoding
@@ -34,5 +56,6 @@
 
     angular
         .module('app', ['ngResource', 'ngCookies'])
+        .config(["$locationProvider", $locationProvider => $locationProvider.html5Mode(true)])
         .controller('mainController', MainController);
 }
