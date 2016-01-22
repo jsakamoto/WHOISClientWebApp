35fbc6ffe0654354e4d5c50b42bd93cf3bc4be46
jsakamoto <junichi.jp@gmail.com>
2016-01-22 22:00:39 +0900

    Add missing XML document comment.

--- WhoisASPNET/server/models/IPAddressRangeWrapper.cs
+++ WhoisASPNET/server/models/IPAddressRangeWrapper.cs
@@ -6,14 +6,26 @@ using NetTools;
 
 namespace WhoisASPNET
 {
+    /// <summary>
+    /// Wrpper class for IPAddressRange.
+    /// </summary>
     public class IPAddressRangeWrapper
     {
         private IPAddressRange _IPAddressRange;
 
+        /// <summary>
+        /// Begin of IP address in range.
+        /// </summary>
         public string Begin => _IPAddressRange?.Begin.ToString();
 
+        /// <summary>
+        /// End of IP address in range.
+        /// </summary>
         public string End => _IPAddressRange?.End.ToString();
 
+        /// <summary>
+        /// Wrpper class for IPAddressRange.
+        /// </summary>
         public IPAddressRangeWrapper(IPAddressRange ipaddressRange)
         {
             _IPAddressRange = ipaddressRange;
--- WhoisASPNET/server/models/WhoisResponseWrapper.cs
+++ WhoisASPNET/server/models/WhoisResponseWrapper.cs
@@ -6,18 +6,36 @@ using Whois.NET;
 
 namespace WhoisASPNET
 {
+    /// <summary>
+    /// Wrpper class for WhoisResponse.
+    /// </summary>
     public class WhoisResponseWrapper
     {
         private WhoisResponse _Response;
 
+        /// <summary>
+        /// Organization name.
+        /// </summary>
         public string OrganizationName  => _Response?.OrganizationName;
 
+        /// <summary>
+        /// Raw response text of WHOIS protocol.
+        /// </summary>
         public string Raw => _Response?.Raw;
 
+        /// <summary>
+        /// Responded servers host name.
+        /// </summary>
         public string[] RespondedServers => _Response?.RespondedServers ?? new string[0];
 
+        /// <summary>
+        /// Range of IP address.
+        /// </summary>
         public IPAddressRangeWrapper AddressRange { get; private set; }
 
+        /// <summary>
+        /// Wrpper class for WhoisResponse.
+        /// </summary>
         public WhoisResponseWrapper(WhoisResponse response)
         {
             _Response = response;
